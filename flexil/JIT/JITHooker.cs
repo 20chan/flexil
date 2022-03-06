044f9e53b8f1723a1021fc8260c1fe897e971f59
20chan <2@0chan.dev>
2022-03-06 19:39:54 +0900

    init working project

---
+++ flexil/JIT/JITHooker.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using static flexil.JIT.WinAPI;
+
+namespace flexil.JIT {
+  public class JITHooker {
+    public bool IsHooked { get; private set; }
+    public Guid CLRVersionID { get; private set; }
+
+    private IntPtr _jitAddr;
+    private CompileMethodDelegate _originalCompileMethod;
+    private CompileMethodDelegate _replacedCompileMethod;
+
+    private MethodInfo[] targetMethods;
+
+    public JITHooker() {
+      IsHooked = false;
+    }
+
+    public bool Hook() {
+      var methods = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(a => a.GetTypes().SelectMany(b => b.GetMethods().Where(t => t.IsDefined(typeof(HookHereAttribute), false))));
+
+
+
+      foreach (ProcessModule module in Process.GetCurrentProcess().Modules) {
+        if (Path.GetFileName(module.FileName) == "clrjit.dll") {
+          var jitAddr = GetProcAddress(module.BaseAddress, "getJit");
+          if (jitAddr != IntPtr.Zero) {
+            ReplaceJit(jitAddr);
+            return IsHooked;
+          }
+        }
+      }
+      return false;
+    }
+
+    public bool UnHook() {
+      throw new NotImplementedException();
+    }
+
+
+    private void ReplaceJit(IntPtr jitAddr) {
+      _jitAddr = jitAddr;
+      var getJit = Marshal.GetDelegateForFunctionPointer<GetJitDelegate>(jitAddr);
+      var jit = getJit();
+      var jitTable = Marshal.ReadIntPtr(jit);
+      var getVerIdPtr = Marshal.ReadIntPtr(jitTable, IntPtr.Size * 4);
+      var getVerId = Marshal.GetDelegateForFunctionPointer<GetVersionIdentifierDelegate>(getVerIdPtr);
+      getVerId(jitAddr, out var guid);
+      CLRVersionID = guid;
+
+      var compileMethodPtr = Marshal.ReadIntPtr(jitTable, 0);
+      _originalCompileMethod = Marshal.GetDelegateForFunctionPointer<CompileMethodDelegate>(compileMethodPtr);
+      _replacedCompileMethod = CompileMethod;
+      var replacedCompileMethodPtr = Marshal.GetFunctionPointerForDelegate(_replacedCompileMethod);
+
+      var trampolinePtr = AllocateTrampoline(replacedCompileMethodPtr);
+      var trampoline = Marshal.GetDelegateForFunctionPointer<CompileMethodDelegate>(trampolinePtr);
+      var emptyInfo = default(CORINFO_METHOD_INFO);
+
+      trampoline(IntPtr.Zero, IntPtr.Zero, ref emptyInfo, 0, out var _, out var _);
+      FreeTrampoline(trampolinePtr);
+
+      VirtualProtect(jitTable, new IntPtr(IntPtr.Size), MemoryProtection.ReadWrite, out var oldFlags);
+      Marshal.WriteIntPtr(jitTable, 0, replacedCompileMethodPtr);
+      VirtualProtect(jitTable, new IntPtr(IntPtr.Size), oldFlags, out _);
+
+      IsHooked = true;
+    }
+
+    readonly byte[] DelegateTrampolineCode = {
+            // mov rax, 0000000000000000h ;
+            0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            // jmp rax
+            0xFF, 0xE0
+        };
+
+    private IntPtr AllocateTrampoline(IntPtr dest) {
+      var jmp = VirtualAlloc(IntPtr.Zero, DelegateTrampolineCode.Length, AllocationType.Commit, MemoryProtection.ExecuteReadWrite);
+      Marshal.Copy(DelegateTrampolineCode, 0, jmp, DelegateTrampolineCode.Length);
+      Marshal.WriteIntPtr(jmp, 2, dest);
+      return jmp;
+    }
+
+    private void FreeTrampoline(IntPtr trampoline) {
+      VirtualFree(trampoline, new IntPtr(DelegateTrampolineCode.Length), FreeType.Release);
+    }
+
+
+    private static readonly object jitLock = new object();
+    private static readonly Dictionary<IntPtr, Assembly> assemblies = new Dictionary<IntPtr, Assembly>();
+
+    int i = 0;
+
+    private int CompileMethod(
+        IntPtr thisPtr,
+        IntPtr comp,
+        ref CORINFO_METHOD_INFO info,
+        uint flags,
+        out IntPtr nativeEntry,
+        out int nativeSizeOfCode) {
+      if (!IsHooked) {
+        nativeEntry = IntPtr.Zero;
+        nativeSizeOfCode = 0;
+        return 0;
+      }
+
+      // https://github.com/xoofx/ManagedJit/blob/master/ManagedJit/ManagedJit.cs
+      var res = _originalCompileMethod(thisPtr, comp, ref info, flags, out nativeEntry, out nativeSizeOfCode);
+
+      if (i++ != 0) {
+        return res;
+			}
+
+      var vtableCorJitInfo = Marshal.ReadIntPtr(comp);
+
+      var getMethodDefFromMethodPtr = Marshal.ReadIntPtr(vtableCorJitInfo, IntPtr.Size * 116);
+      var getMethodDefFromMethod = Marshal.GetDelegateForFunctionPointer<GetMethodDefFromMethodDelegate>(getMethodDefFromMethodPtr);
+      var methodToken = getMethodDefFromMethod(comp, info.ftn);
+
+      var getModuleAssemblyDelegatePtr = Marshal.ReadIntPtr(vtableCorJitInfo, IntPtr.Size * 48);
+      var getModuleAssemblyDelegate = Marshal.GetDelegateForFunctionPointer<GetModuleAssemblyDelegate>(getModuleAssemblyDelegatePtr);
+      var assemblyHandle = getModuleAssemblyDelegate(comp, info.scope);
+
+      Assembly assembly = null;
+      lock (jitLock) {
+        if (!assemblies.TryGetValue(assemblyHandle, out assembly)) {
+          var getAssemblyNamePtr = Marshal.ReadIntPtr(vtableCorJitInfo, IntPtr.Size * 49);
+          var getAssemblyName = Marshal.GetDelegateForFunctionPointer<GetAssemblyNameDelegate>(getAssemblyNamePtr);
+          var assemblyNamePtr = getAssemblyName(comp, assemblyHandle);
+
+          var assemblyName = Marshal.PtrToStringAnsi(assemblyNamePtr);
+
+          foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
+            if (asm.GetName().Name == assemblyName) {
+              assembly = asm;
+              break;
+						}
+					}
+
+          assemblies.Add(assemblyHandle, assembly);
+				}
+			}
+
+      if (assembly != null) {
+        MethodBase method = null;
+        foreach (var module in assembly.Modules) {
+          try {
+            method = module.ResolveMethod(methodToken);
+					} catch {
+					}
+				}
+
+        if (method != null) {
+          HandleMethodCompile(method, info.ILCode, info.ILCodeSize, nativeEntry, nativeSizeOfCode);
+        }
+			}
+
+      return res;
+    }
+
+    private void HandleMethodCompile(MethodBase method, IntPtr ilCodePtr, int ilSize, IntPtr nativeCodePtr, int nativeCodeSize) {
+      if (method.Name == "Answer") {
+        var codes = new byte[nativeCodeSize];
+        // Marshal.Copy(nativeCodePtr, codes, 0, nativeCodeSize);
+        // Console.WriteLine(BitConverter.ToString(codes));
+        Marshal.WriteByte(nativeCodePtr, 0x25, 2);
+      }
+    }
+  }
+}
---
+++ flexil/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using flexil.JIT;
+
+namespace flexil {
+	class Program {
+		static void Main(string[] args) {
+			var hooker = new JITHooker();
+			if (hooker.Hook()) {
+				Console.WriteLine("hook sucucess");
+			}
+
+			Console.WriteLine(Answer());
+			Console.Read();
+		}
+
+		[HookHere]
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		static int Answer() {
+			/*
+				0:  55                      push   ebp
+				1:  57                      push   edi
+				2:  56                      push   esi
+				3:  48                      dec    eax
+				4:  83 ec 30                sub    esp,0x30
+				7:  48                      dec    eax
+				8:  8b ec                   mov    ebp,esp
+				a:  33 c0                   xor    eax,eax
+				c:  89 45 24                mov    DWORD PTR [ebp+0x24],eax
+				f:  48                      dec    eax
+				10: 89 45 28                mov    DWORD PTR [ebp+0x28],eax
+				13: 83 3d 46 6e 09 00 00    cmp    DWORD PTR ds:0x96e46,0x0
+				1a: 74 05                   je     0x21
+				1c: e8 4f 4b c2 5f          call   0x5fc24b70
+				21: 90                      nop
+				22: c7 45 24 ff 00 00 00    mov    DWORD PTR [ebp+0x24],0xff ; <- 0xff 부분, addr 0x25를 치환
+				29: 90                      nop
+				2a: eb 00                   jmp    0x2c
+				2c: 8b 45 24                mov    eax,DWORD PTR [ebp+0x24]
+				2f: 48                      dec    eax
+				30: 8d 65 30                lea    esp,[ebp+0x30]
+				33: 5e                      pop    esi
+				34: 5f                      pop    edi
+				35: 5d                      pop    ebp
+				36: c3                      ret
+			 */
+
+			return 1;
+		}
+	}
+}
